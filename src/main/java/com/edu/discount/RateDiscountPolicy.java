8bd7a2139a9468ec3035978434cc1d6de420fae4
ajm <jeck77@naver.com>
2023-01-08 17:28:34 +0900

    할인 비율 정책 생성

--- src/main/java/com/edu/discount/RateDiscountPolicy.java
+++ src/main/java/com/edu/discount/RateDiscountPolicy.java
@@ -1,4 +1,16 @@
 package com.edu.discount;
 
-public class RateDiscountPolicy {
+import com.edu.member.Grade;
+import com.edu.member.Member;
+
+public class RateDiscountPolicy implements DiscountPolicy {
+    private  int discountPercent = 10;
+    @Override
+    public int discount(Member member, int price) {
+        if(member.getGrade() == Grade.VIP){
+            return price * discountPercent / 100;
+        }else{
+            return 0;
+        }
+    }
 }
