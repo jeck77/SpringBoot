ef06b05c45ade6be0afb3aba2d0f25ec4e4e36e8
ajm <jeck77@naver.com>
2023-01-08 16:53:55 +0900

    수정

--- src/test/java/com/edu/order/OrderServiceTest.java
+++ src/test/java/com/edu/order/OrderServiceTest.java
@@ -19,7 +19,6 @@ public class OrderServiceTest {
         memberService.join(member);
 
         Order order = orderService.createOrder(memberId, "itemA", 10000);
-
         Assertions.assertThat(order.getDiscountPrice()).isEqualTo(1000);
     }
 }
